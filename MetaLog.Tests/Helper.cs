f500a2926955ff1f9f88f0bc9c4abc653f29951a
mrousavy <mrousavy@donotreply.github.com>
2017-09-01 20:17:42 +0200

    Add Exception builder for Tests

---
+++ MetaLog.Tests/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaLog.Tests {
+    public static class Helper {
+        public static Exception BuildException() {
+            return new Exception("Outest Exception", new Exception("Some inner", new Exception("Innest Exceptin")));
+        }
+    }
+}
