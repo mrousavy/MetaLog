8912b8996b52550a99b93340fadd915d8279bed6
mrousavy <mrousavy@donotreply.github.com>
2017-09-01 18:38:42 +0200

    A static logger implementation

--- MetaLog/Logger.cs
+++ MetaLog/Logger.cs
@@ -1,8 +1,66 @@
 ﻿using System;
+using System.IO;
 
-namespace MetaLog
-{
-    public class Logger
-    {
+namespace MetaLog {
+    /// <summary>
+    /// A static <see cref="ILogger"/>
+    /// </summary>
+    public static class Logger {
+        private static bool _useStream;
+        private static string _logFile;
+
+        private static object Lock { get; } = new object();
+
+        /// <summary>
+        /// The <see cref="FileStream"/> used for writing to the LogFile
+        /// </summary>
+        private static FileStream FileStream { get; set; }
+        /// <summary>
+        /// The log file this <see cref="ILogger"/> instance logs to
+        /// (Setting this to null will close the Stream if 
+        /// <see cref="Logger.UseStream"/> is set to true
+        /// </summary>
+        public static string LogFile {
+            get => _logFile;
+            set {
+                if (value == _logFile) return; //don't reopen stream if it's same value
+                _logFile = value;
+            }
+        }
+        /// <summary>
+        /// Whether this <see cref="ILogger"/> uses a single
+        /// (File)-<see cref="Stream"/> for logging, or opens new
+        /// ones each log. (<see cref="Stream"/>s may be faster, but
+        /// locks the file until the <see cref="ILogger"/> gets disposed, see: 
+        /// <see href="https://en.wikipedia.org/wiki/File_locking">file locking</see>)
+        /// </summary>
+        public static bool UseStream { get; set; }
+        /// <summary>
+        /// The minimum <see cref="LogSeverity"/> to log by this Logger instance
+        /// (It is recommended to use higher values such as <see cref="LogSeverity.Error"/>
+        /// for releases)
+        /// </summary>
+        public static LogSeverity MinimumSeverity { get; set; }
+
+
+
+        /// <summary>
+        /// Log a new message to the <see cref="LogFile"/>
+        /// </summary>
+        /// <param name="severity">The <see cref="LogSeverity"/> of this message</param>
+        /// <param name="message">The actual log-message</param>
+        public static void Log(LogSeverity severity, string message) {
+            
+        }
+
+        /// <summary>
+        /// Log a new <see cref="Exception"/> tree (up to most 
+        /// inner <see cref="Exception"/>) to the <see cref="LogFile"/>
+        /// </summary>
+        /// <param name="severity">The <see cref="LogSeverity"/> of this message</param>
+        /// <param name="exception">An occured <see cref="Exception"/></param>
+        public static void Log(LogSeverity severity, Exception exception) {
+            
+        }
     }
 }
