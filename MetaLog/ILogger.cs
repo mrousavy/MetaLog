b01f79d195a361b3452aafa107410c5173550568
mrousavy <mrousavy@donotreply.github.com>
2017-09-01 18:10:27 +0200

    More documentation for ILogger

--- MetaLog/ILogger.cs
+++ MetaLog/ILogger.cs
@@ -1,4 +1,7 @@
-﻿namespace MetaLog {
+﻿using System;
+using System.IO;
+
+namespace MetaLog {
 
     /// <summary>
     /// A MetaLog logging severity enum
@@ -33,12 +36,20 @@
     /// <summary>
     /// A MetaLog logger
     /// </summary>
-    public interface ILogger
+    public interface ILogger : IDisposable
     {
         /// <summary>
         /// The log file this <see cref="ILogger"/> instance logs to
         /// </summary>
         string LogFile { get; }
+        /// <summary>
+        /// Whether this <see cref="ILogger"/> should use a single
+        /// (File)-<see cref="Stream"/> for logging instead of opening new
+        /// ones each log. (This may be faster but locks the file
+        /// until the <see cref="ILogger"/> gets disposed, see: 
+        /// <see href="https://en.wikipedia.org/wiki/File_locking">file locking</see>)
+        /// </summary>
+        bool UseStream { get; }
         /// <summary>
         /// The minimum <see cref="LogSeverity"/> to log by this Logger instance
         /// (It is recommended to use higher values such as <see cref="LogSeverity.Error"/>
@@ -52,5 +63,12 @@
         /// <param name="severity">The <see cref="LogSeverity"/> of this message</param>
         /// <param name="message">The actual log-message</param>
         void Log(LogSeverity severity, string message);
+        /// <summary>
+        /// Log a new <see cref="Exception"/> tree (up to most 
+        /// inner <see cref="Exception"/>) to the <see cref="LogFile"/>
+        /// </summary>
+        /// <param name="severity">The <see cref="LogSeverity"/> of this message</param>
+        /// <param name="exception">An occured <see cref="Exception"/></param>
+        void Log(LogSeverity severity, Exception exception);
     }
 }
