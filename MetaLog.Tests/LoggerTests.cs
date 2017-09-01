534cc4e44fe7817fa7b6cfbb68261ab23dc1c307
mrousavy <mrousavy@donotreply.github.com>
2017-09-01 20:17:56 +0200

    Add Logger static and init tests template

---
+++ MetaLog.Tests/LoggerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MetaLog.Tests {
+    [TestClass]
+    public class LoggerTests {
+        public static ILogger logger;
+
+
+        [TestMethod]
+        public void TestLog1() {
+
+        }
+        [TestMethod]
+        public void TestLog2() {
+
+        }
+        [TestMethod]
+        public void TestLog3() {
+
+        }
+        [TestMethod]
+        public void TestLog4() {
+
+        }
+    }
+}
---
+++ MetaLog.Tests/StaticTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MetaLog.Tests {
+    [TestClass]
+    public class StaticTests {
+
+        [TestInitialize]
+        public void Initialize() {
+            MetaLog.Logger.UseStream = true;
+            MetaLog.Logger.Encoding = Encoding.ASCII;
+        }
+
+        [TestMethod]
+        public void TestStaticLog1() {
+            MetaLog.Logger.Log(MetaLog.LogSeverity.Info, "Starting Test 1.");
+            MetaLog.Logger.Log(MetaLog.LogSeverity.Info, Helper.BuildException());
+            MetaLog.Logger.Log(MetaLog.LogSeverity.Info, "Test 1 is about to finish.");
+        }
+        [TestMethod]
+        public void TestStaticLog2() {
+
+        }
+        [TestMethod]
+        public void TestStaticLog3() {
+
+        }
+        [TestMethod]
+        public void TestStaticLog4() {
+
+        }
+    }
+}
