5c115791dc054690b1ca1dc40359cd26e2b8bc34
mrousavy <mrousavy@donotreply.github.com>
2017-09-01 18:02:48 +0200

    Add enum and docs to Logger interface

--- MetaLog/ILogger.cs
+++ MetaLog/ILogger.cs
@@ -1,10 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace MetaLog {
+    public enum LogSeverity {
+        Debug,
+        Info,
+        Warning,
+        Error,
+        Critical
+    }
 
-namespace MetaLog
-{
+    /// <summary>
+    /// A MetaLog logger
+    /// </summary>
     public interface ILogger
     {
+        /// <summary>
+        /// The log file this <see cref="ILogger"/> instance logs to
+        /// </summary>
+        string LogFile { get; }
+        /// <summary>
+        /// The minimum <see cref="LogSeverity"/> to log by this Logger instance
+        /// (It is recommended to use higher values such as <see cref="LogSeverity.Error"/>
+        /// for releases)
+        /// </summary>
+        LogSeverity MinimumSeverity { get; }
+
+        /// <summary>
+        /// Log a new message to the <see cref="LogFile"/>
+        /// </summary>
+        /// <param name="severity">The <see cref="LogSeverity"/> of this message</param>
+        /// <param name="message">The actual log-message</param>
+        void Log(LogSeverity severity, string message);
     }
 }
