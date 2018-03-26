4df0292241423a972093c1c631091bb0d8b49def
mrousavy <mrousavy@donotreply.github.com>
2018-03-26 19:15:31 +0200

    Add WPF test app

---
+++ MetaLog.WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MetaLog.WpfTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ILogger Logger { get; } = 
+            new MetaLogger(Utilities.RecommendedLogFile);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ThrowExceptionClick(object sender, RoutedEventArgs e)
+        {
+            var thread = new Thread(() =>
+            {
+                try
+                {
+                    try
+                    {
+                        ((Button) sender).Content = "Try to change UI from Thread?.. whoops";
+                    } catch(Exception inner)
+                    {
+                        throw new Exception("This is some outer exception", inner);
+                    }
+                } catch(Exception ex)
+                {
+                    Logger.Log(LogSeverity.Error, ex);
+                }
+            });
+            thread.Start();
+        }
+    }
+}
