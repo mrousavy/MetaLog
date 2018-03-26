f16efdf33c18c0014e2dc65fcb27fb3affa28c4a
mrousavy <mrousavy@donotreply.github.com>
2018-03-26 19:04:44 +0200

    Add NullOrWhitespace checker extension func

---
+++ MetaLog/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetaLog
+{
+    public static class Extensions
+    {
+        /// <summary>
+        ///     Validate this string if it is not null or whitespace
+        /// </summary>
+        public static bool IsValid(this string str)
+        {
+            return !string.IsNullOrWhiteSpace(str);
+        }
+    }
+}
